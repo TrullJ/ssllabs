a27bb65c41282bf724d965ba65855454d44fc260
Lewis Juggins <ldjuggins@gmail.com>
2016-08-21 10:45:13 +0100

    Quote consistency

--- ssllabsscanner.py
+++ ssllabsscanner.py
@@ -5,30 +5,30 @@ import time
 import sys
 import logging
 
-API = "https://api.ssllabs.com/api/v2/"
+API = 'https://api.ssllabs.com/api/v2/'
 
 
 def requestAPI(path, payload={}):
-    """This is a helper method that takes the path to the relevant
+    '''This is a helper method that takes the path to the relevant
         API call and the user-defined payload and requests the
         data/server test from Qualys SSL Labs.
 
-        Returns JSON formatted data"""
+        Returns JSON formatted data'''
 
     url = API + path
 
     try:
         response = requests.get(url, params=payload)
     except requests.exception.RequestException:
-        logging.exception("Request failed.")
+        logging.exception('Request failed.')
         sys.exit(1)
 
     data = response.json()
     return data
 
 
-def resultsFromCache(host, publish="off", startNew="off", fromCache="on", all="done"):
-    path = "analyze"
+def resultsFromCache(host, publish='off', startNew='off', fromCache='on', all='done'):
+    path = 'analyze'
     payload = {
                 'host': host,
                 'publish': publish,
@@ -40,8 +40,8 @@ def resultsFromCache(host, publish="off", startNew="off", fromCache="on", all="d
     return data
 
 
-def newScan(host, publish="off", startNew="on", all="done", ignoreMismatch="on"):
-    path = "analyze"
+def newScan(host, publish='off', startNew='on', all='done', ignoreMismatch='on'):
+    path = 'analyze'
     payload = {
                 'host': host,
                 'publish': publish,
